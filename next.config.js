03ffff0ae1a8817de4f32f2ce8c9ed7356606c2b
Anwar BinUmer <anbinumer@users.noreply.github.com>
2025-06-14 08:33:30 +1000

    Update Next.js configuration for LTI launch route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/lti/launch',
+        destination: '/app/lti/launch',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
