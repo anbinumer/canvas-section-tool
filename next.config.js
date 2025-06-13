aa34d9a37676a7f61710e1ab43e3377addf810b6
Anwar BinUmer <anbinumer@users.noreply.github.com>
2025-06-14 08:36:55 +1000

    Simplify routing configuration

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/lti/launch',
-        destination: '/app/lti/launch',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig 
